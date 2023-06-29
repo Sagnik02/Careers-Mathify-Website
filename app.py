368e11fd797a43ea673e6123aa2c627ca228d357
Sagnik02 <108208974+Sagnik02@users.noreply.github.com>
2023-06-29 03:35:56 +0000

    Added dynamic data

--- app.py
+++ app.py
@@ -2,10 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+  'id': 1,
+  'title': 'Calculus Mentor',
+  'location': 'Bengaluru, India',
+  'salary': 'Rs. 4,00,00'
+}, {
+  'id': 2,
+  'title': 'Subject Matter Expert',
+  'location': 'Kolkata, India',
+  'salary': 'Rs. 25,00,00'
+}, {
+  'id': 3,
+  'title': 'Coordinate Geometry Expert',
+  'location': 'Remote'
+}, {
+  'id': 4,
+  'title': 'Frontend Developer',
+  'location': 'Hyderabad, India',
+  'salary': '1150,000'
+}]
+
 
 @app.route('/')
 def hello_mathify():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name='Mathify')
 
 
 if __name__ == '__main__':
